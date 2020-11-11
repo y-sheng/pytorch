0d3f9fef12c756a8c4f704b65a7c784d575a581d
Christian Puhrsch <cpuhrsch@devfair0129.h2.fair>
2020-11-11 12:40:03 -0800

    More autograd extension

--- aten/src/ATen/native/TensorShape.cpp
+++ aten/src/ATen/native/TensorShape.cpp
@@ -641,12 +641,19 @@ Tensor expand(const Tensor& self, IntArrayRef size, bool implicit) {
   return result;
 }
 
+Tensor expand_nt(const Tensor& self, const Tensor& nested_size, bool implicit) {
+  TORCH_CHECK(false, "expand_nt NOT IMPLEMENTED.");
+  return self;
+}
+
 Tensor expand_as(const Tensor& self, const Tensor& other) {
+  if (at::is_nested_tensor_impl(other)) {
+    return at::expand_nt(self, at::serialize_nested_size(other));
+  }
   return self.expand(other.sizes());
 }
 
-Tensor sum_to_size(const Tensor& self, IntArrayRef size) {
-  TORCH_CHECK(is_expandable_to(size, self.sizes()),
+Tensor sum_to_size(const Tensor& self, IntArrayRef size) { TORCH_CHECK(is_expandable_to(size, self.sizes()),
            "size {", size, "} is not expandable to size {", self.sizes(), "}.");
 
   return at::native::sum_to(self, size);
@@ -1906,6 +1913,28 @@ Tensor movedim(const Tensor& self, IntArrayRef src, IntArrayRef dst) {
 
 Tensor movedim(const Tensor& self, int64_t src, int64_t dst) {
   return at::movedim(self, IntArrayRef{src}, IntArrayRef{dst});
+}
+
+Tensor serialize_nested_size(const Tensor& tensor) {
+  return at::empty({}, at::kLong);
+}
+
+bool sizes_equal_nt_other(const Tensor& self, IntArrayRef size_other) {
+  TORCH_CHECK(false, "sizes_equal_nt_other NOT IMPLEMENTED.");
+  return false;
+}
+
+bool sizes_equal(const Tensor& self, IntArrayRef size_other) {
+  return self.sizes().equals(size_other);
+}
+
+bool native_is_expandable_to_nt_other(IntArrayRef metadata_nested_size, const Tensor& grad) {
+  TORCH_CHECK(false, "native_is_expandable_to_nt_other NOT IMPLEMENTED.");
+  return false;
+}
+
+bool native_is_expandable_to(IntArrayRef metadata_shape, const Tensor& grad) {
+  return at::is_expandable_to(metadata_shape, grad.sizes());
 }
 
 }} // at::native
